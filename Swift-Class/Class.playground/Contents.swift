6b662c460eebe5fb3147d391d8cd71ab2212d9bd
wenmobo <wenmobo2018@gmail.com>
2019-08-01 23:49:50 +0800

    提交classdemo

---
+++ Swift-Class/Class.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+import Foundation
+
+class Point2D {
+    var x: Double = 0
+    var y: Double = 0
+    
+    // Designated init
+    init(x: Double = 0, y: Double = 0) {
+        self.x = x
+        self.y = y
+    }
+    
+    // Convenience init
+    convenience init(at: (Double, Double)) {
+        self.init(x: at.0, y: at.1)
+    }
+    
+    //Failable init
+    convenience init?(at: (String, String)) {
+        guard let x = Double(at.0), let y = Double(at.1) else {
+            return nil
+        }
+        
+        self.init(at: (x, y))
+    }
+}
+
+let point1 = Point2D(at: (1, 1))
+print(point1)
+
+class Point3D: Point2D {
+    var z: Double
+}
