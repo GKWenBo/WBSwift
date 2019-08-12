fb58539f9c6814eebaa1799c4ebe4fcc386687e3
wenmobo <wenmobo2018@gmail.com>
2019-08-13 00:36:35 +0800

    no message

--- Swift-Class/Class.playground/Contents.swift
+++ Swift-Class/Class.playground/Contents.swift
@@ -30,5 +30,25 @@ let point1 = Point2D(at: (1, 1))
 print(point1)
 
 class Point3D: Point2D {
-    var z: Double
+    var z: Double = 0
+    
+    //重载init方法
+    init(x: Double = 0, y: Double = 0, z: Double = 0) {
+        self.z = z
+        super.init(x: x, y: y)
+    }
+    
+    override init(x: Double = 0, y: Double = 0) {
+        self.z = 0
+        super.init(x: x, y: y)
+    }
+    
+    // Convenience init
+    convenience init(at: (Double, Double)) {
+        self.init(x: at.0, y: at.1, z: 0)
+    }
 }
+
+let origin3D = Point3D()
+let point11 = Point3D(x: 1, y: 1)
+let point12 = Point3D(at: (0, 1))
