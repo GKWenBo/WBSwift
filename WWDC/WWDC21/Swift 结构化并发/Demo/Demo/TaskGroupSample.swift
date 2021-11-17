dc310713f6c4975aba04b344a7d0f78854bf7ba5
wenbo <wenbo22@staff.weibo.com>
2021-11-17 19:41:13 +0800

    add demo

--- WWDC/WWDC21/Swift 结构化并发/Demo/Demo/TaskGroupSample.swift
+++ WWDC/WWDC21/Swift 结构化并发/Demo/Demo/TaskGroupSample.swift
@@ -6,3 +6,144 @@
 //
 
 import Foundation
+
+// 任务组
+struct TaskGroupSample {
+    func start() async {
+        print("Start")
+        
+        // 1
+        await withTaskGroup(of: Int.self, body: { group in
+            for i in 0..<3 {
+                group.addTask {
+                    await work(i)
+                }
+            }
+            
+            print("Task added")
+            
+            // 4
+            for await result in group {
+                print("Get result: \(result)")
+            }
+            
+            // 5
+            print("Task ended")
+            
+        })
+        
+        print("End")
+    }
+    
+    // MARK: - 隐式等待
+    /*
+     为了获取子任务的结果，我们在上例中使用 for await 明确地等待 group 完成。这从语义上明确地满足结构化并发的要求：子任务会在控制流到达底部前结束。不过一个常见的疑问是，其实编译器并没有强制我们书写 for await 代码。如果我们因为某种原因，比如由于用不到这些结果，而导致忘了等待 group，会发生什么呢？任务组会不会因为没有等待，而导致原来的控制流不会暂停，就这样继续运行并结束？这样是不是违反了结构化并发的需要？
+
+     好消息是，即使我们没有明确 await 任务组，编译器在检测到结构化并发作用域结束时，会为我们自动添加上 await 并在等待所有任务结束后再继续控制流。比如，在上面的代码中，如果我们将 for await 部分删去：
+     */
+    func start1() async {
+        print("Start")
+        
+        // 1
+        await withTaskGroup(of: Int.self, body: { group in
+            for i in 0..<3 {
+                group.addTask {
+                    await work(i)
+                }
+            }
+            
+            print("Task added")
+            
+            // 4
+//            for await result in group {
+//                print("Get result: \(result)")
+//            }
+            
+            // 编译器自动生成的代码
+            for await _ in group { }
+            
+            // 5
+            print("Task ended")
+            
+        })
+        
+        print("End")
+    }
+    
+    func start2() async {
+        print("Start")
+        
+        // 1
+        await withTaskGroup(of: Int.self, body: { group in
+            for i in 0..<3 {
+                group.addTask {
+                    await work(i)
+                }
+            }
+            
+            print("Task added")
+            
+            for await result in group {
+                print("Get result: \(result)")
+                // 在首个子任务完成后就跳出
+                break
+            }
+            
+            // 5
+            print("Task ended")
+            
+            // 编译器自动生成代码
+            await group.waitForAll()
+            
+        })
+        
+        print("End")
+    }
+    
+    // MARK: - 任务组的值捕获
+    /*
+     任务组中的每个子任务都拥有返回值，上面例子中 work 返回的 Int 就是子任务的返回值。当 for await 一个任务组时，就可以获取到每个子任务的返回值。任务组必须在所有子任务完成后才能完成，因此我们有机会“整理”所有子任务的返回结果，并为整个任务组设定一个返回值。比如把所有的 work 结果加起来：
+     */
+    func test() async {
+        let v: Int = await withTaskGroup(of: Int.self) { group in
+            var value = 0
+            for i in 0..<3 {
+                return await work(i)
+            }
+            
+            for await result in group {
+                value += result
+            }
+            
+            return value
+        }
+        
+        print("End. Result: \(v)")
+    }
+    
+    // MARK: - 任务组逃逸
+    /*
+     和 withUnsafeCurrentTask 中的 task 类似，withTaskGroup 闭包中的 group 也不应该被外部持有并在作用范围之外使用。虽然 Swift 编译器现在没有阻止我们这样做，但是在 withTaskGroup 闭包外使用 group 的话，将完全破坏结构化并发的假设：
+     */
+    func start3() async {
+        var g: TaskGroup<Int>? = nil
+        await withTaskGroup(of: Int.self, body: { group in
+            g = group
+            
+            g?.addTask {
+                await work(1)
+            }
+            
+            print("End")
+        })
+    }
+    
+    func work(_ value: Int) async -> Int {
+        // 3
+        print("Start work \(value)")
+        await Task.sleep(UInt64(value) * NSEC_PER_SEC)
+        print("Work \(value) done")
+        return value
+    }
+    
+}
--- WWDC/WWDC21/Swift 结构化并发/Demo/Demo/ViewController.swift
+++ WWDC/WWDC21/Swift 结构化并发/Demo/Demo/ViewController.swift
@@ -17,7 +17,15 @@ class ViewController: UIViewController {
         
 //        test()
         
-        test()
+//        test()
+        
+//        Task {
+//            await testGroup()
+//        }
+        
+        Task {
+            await start()
+        }
     }
 
     // MARK: - 非结构化的并发
@@ -50,6 +58,12 @@ class ViewController: UIViewController {
      */
     
     // MARK: - 基于 Task 的结构化并发模型
+    /*
+     1.withUnsafeCurrentTask 本身不是异步函数，你也可以在普通的同步函数中使用它。如果当前的函数并没有运行在任何任务上下文环境中，也就是说，到 withUnsafeCurrentTask 为止的调用链中如果没有异步函数的话，这里得到的 task 会是 nil。
+     2.使用 Task 的初始化方法，可以得到一个新的任务环境。在上一章中我们已经看到过几种开始任务的方式了。
+     3.对于 foo 的调用，发生在上一步的 Task 闭包作用范围中，它的运行环境就是这个新创建的 Task。
+     4.对于获取到的 task，可以访问它的 isCancelled 和 priority 属性检查它是否已经被取消以及当前的优先级。我们甚至可以调用 cancel() 来取消这个任务。
+     */
 //    func test() {
 //        withUnsafeCurrentTask { task in
 //            // 1
@@ -86,6 +100,7 @@ class ViewController: UIViewController {
 //        }
 //    }
     
+    
     // MARK: - 任务层级
     /*
      上例中虽然 t1 和 t2 是在外层 Task 中再新生成并进行并发的，但是它们之间没有从属关系，并不是结构化的。这一点从 t: false 先于其他输出就可以看出，t1 和 t2 的执行都是在外层 Task 闭包结束后才进行的，它们逃逸出去了，这和结构化并发的收束规定不符。
@@ -104,19 +119,148 @@ class ViewController: UIViewController {
     /*
      虽然被定义为 static var，但是它们并不表示针对所有 Task 类型通用的某个全局属性，而是表示当前任务的情况。因为一个异步函数的运行环境必须有且仅会有一个任务上下文，所以使用 static 变量来表示这唯一一个任务的特性，是可以理解的。相比于每次去获取 UnsafeCurrentTask，这种写法更加简单。
      */
-    func test() {
-        Task {
-            let t1 = Task {
-                print("t1: \(Task.isCancelled)")
-            }
-            
-            let t2 = Task {
-                print("t2: \(Task.isCancelled)")
-            }
-            
-            t1.cancel()
-            print("t: \(Task.isCancelled)")
-        }
+//    func test() {
+//        Task {
+//            let t1 = Task {
+//                print("t1: \(Task.isCancelled)")
+//            }
+//
+//            let t2 = Task {
+//                print("t2: \(Task.isCancelled)")
+//            }
+//
+//            t1.cancel()
+//            print("t: \(Task.isCancelled)")
+//        }
+//    }
+    
+    // MARK: - async let 异步绑定
+//    func start() async {
+//        print("Start")
+//
+//        async let v0 = work(0)
+//        async let v1 = work(1)
+//        async let v2 = work(2)
+//
+//        print("Task added")
+//
+//        let result =  await v0 + v1 + v2
+//        print("Task added")
+//        print("End. Result\(result)")
+//    }
+//
+//    // MARK: - 任务组
+//    func work(_ value: Int) async -> Int {
+//        // 3
+//        print("Start work \(value)")
+//        await Task.sleep(UInt64(value) * NSEC_PER_SEC)
+//        print("Work \(value) done")
+//        return value
+//    }
+//
+//    func testGroup() async {
+////        await TaskGroupSample().start()
+////        await TaskGroupSample().test()
+//        await TaskGroupSample().start3()
+//    }
+    
+    // MARK: - 隐式取消
+    /*
+     在使用 async let 时，编译器也没有强制我们书写类似 await v0 这样的等待语句。有了 TaskGroup 中的经验以及 Swift 里“默认安全”的行为规范，我们不难猜测出，对于没有 await 的异步绑定，编译器也帮我们做了某些“手脚”，以保证单进单出的结构化并发依然成立。
+
+     如果没有 await，那么 Swift 并发会在被绑定的常量离开作用域时，隐式地将绑定的子任务取消掉，然后进行 await。也就是说，对于这样的代码
+     */
+//    func start() async {
+//        async let v0 = work(0)
+//
+//        print("End")
+//    }
+    
+    //  等效于
+//    func start1() async {
+//        async let v0 = work(0)
+//
+//        print("End")
+//        // 下面是编译器自动生成的伪代码
+//        // 注意和 Task group 的不同
+//
+//        // v0 绑定的任务被取消
+//        // 伪代码，实际上绑定中并没有 `task` 这个属性
+//        v0.task?.cancel()
+//        // 隐式 await，满足结构化并发
+//        _ = await v0
+//    }
+    
+    
+    func work(_ value: Int) async -> Int {
+        // 3
+        print("Start work \(value)")
+        await Task.sleep(UInt64(value) * NSEC_PER_SEC)
+        print("Work \(value) done")
+        return value
     }
+    
+    // MARK: - 结构化并发的组合
+    /*
+     在只使用一次 withTaskGroup 或者一组 async let 的单一层级的维度上，我们可能很难看出结构化并发的优势，因为这时对于任务的调度还处于可控状态：我们完全可以使用传统的技术，通过添加一些信号量，来“手动”控制保证并发任务最终可以合并到一起。但是，随着系统逐渐复杂，可能会面临在一些并发的子任务中再次进行任务并发的需求。也就是，形成多个层级的子任务系统。在这种情况下，想依靠原始的信号量来进行任务管理会变得异常复杂。这也是结构化并发这一抽象真正能发挥全部功效的情况。
+
+     通过嵌套使用 withTaskGroup 或者 async let，可以在一般人能够轻易理解的范围内，灵活地构建出这种多层级的并发任务。最简单的方式，是在 withTaskGroup 中为 group 添加 task 时再开启一个 withTaskGroup：
+     */
+//    func start() async {
+//        // 第一任务组
+//        await withTaskGroup(of: Int.self, body: { group in
+//            group.addTask {
+//
+//                // 第二任务组
+//                await withTaskGroup(of: Int.self, body: { innerGroup in
+//                    innerGroup.addTask { [unowned self] in
+//                        await self.work(0)
+//                    }
+//
+//                    innerGroup.addTask { [unowned self] in
+//                        await self.work(2)
+//                    }
+//
+//                    return await innerGroup.reduce(0, { result, value in
+//                        result + value
+//                    })
+//                })
+//            }
+//
+//            group.addTask { [unowned self] in
+//                await self.work(1)
+//            }
+//        })
+//
+//        print("End")
+//    }
+    /*
+     对于上面使用 work 函数的例子来说，多加的一层 innerGroup 在执行时并不会造成太大区别：三个任务依然是按照结构化并发执行。不过，这种层级的划分，给了我们更精确控制并发行为的机会。在结构化并发的任务模型中，子任务会从其父任务中继承任务优先级以及任务的本地值 (task local value)；在处理任务取消时，除了父任务会将取消传递给子任务外，在子任务中的抛出也会将取消向上传递。不论是当我们需要精确地在某一组任务中设置这些行为，或者只是单纯地为了更好的可读性，这种通过嵌套得到更加细分的任务层级的方法，都会对我们的目标有所帮助。
+     */
+    
+    /*
+     相对于 withTaskGroup 的嵌套，使用 async let 会更有技巧性一些。async let 赋值等号右边，接受的是一个对异步函数的调用。这个异步函数可以是像 work 这样的具体具名的函数，也可以是一个匿名函数。比如，上面的 withTaskGroup 嵌套的例子，使用 async let，可以简单地写为：
+     */
+    func start() async {
+        async let v02: Int = {
+            async let v0 = work(0)
+            async let v2 = work(2)
+            return await v0 + v2
+        }()
+        
+        async let v1 = work(1)
+        _ = await v02 + v1
+    }
+    /*
+     这里在 v02 等号右侧的是一个匿名的异步函数闭包调用，其中通过两个新的 async let 开始了嵌套的子任务。特别注意，上例中的写法和下面这样的 await 有本质不同：
+     func start() async {
+       async let v02: Int = {
+         return await work(0) + work(2)
+       }()
+
+       // ...
+     }
+     await work(0) + work(2) 将会顺次执行 work(0) 和 work(2)，并把它们的结果相加。这时两个操作不是并发执行的，也不涉及新的子任务。
+     */
 }
 
